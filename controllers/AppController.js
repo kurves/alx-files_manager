319c4891f0194775945861048496323963f22d46
kurves <kavssimply@gmail.com>
2024-09-29 09:58:08 -0700

    Create a new user

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,5 +1,7 @@
 import redisClient from '../utils/redis.js';
 import dbClient from '../utils/db.js';
+import crypto from 'crypto';
+import dbClient from '../utils/db.js';
 
 class AppController {
   static async getStatus(req, res) {
@@ -20,5 +22,40 @@ class AppController {
   }
 }
 
+class UsersController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
+    }
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+
+    const existingUser = await dbClient.db.collection('users').findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ error: 'Already exist' });
+    }
+
+    const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
+
+    const newUser = {
+      email,
+      password: hashedPassword,
+    };
+
+    try {
+      const result = await dbClient.db.collection('users').insertOne(newUser);
+
+      return res.status(201).json({ id: result.insertedId, email });
+    } catch (error) {
+      console.error('Error creating new user:', error);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+}
+
+export default UsersController;
 export default AppController;
 
