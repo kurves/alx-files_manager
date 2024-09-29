c5da6c3b6199d21485452b3f588b3c394f419e4c
kurves <kavssimply@gmail.com>
2024-09-29 05:08:36 -0700

    MongoDB utils

---
+++ utils/db.js
@@ -0,0 +1,47 @@
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+
+    const url = `mongodb://${host}:${port}`;
+    this.client = new MongoClient(url, { useUnifiedTopology: true });
+    this.client.connect().then(() => {
+      this.db = this.client.db(database);
+    }).catch((err) => {
+      console.error(`Failed to connect to MongoDB: ${err}`);
+    });
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    try {
+      const usersCollection = this.db.collection('users');
+      return await usersCollection.countDocuments();
+    } catch (err) {
+      console.error(`Error getting number of users: ${err}`);
+      return 0;
+    }
+  }
+
+  async nbFiles() {
+    try {
+      const filesCollection = this.db.collection('files');
+      return await filesCollection.countDocuments();
+    } catch (err) {
+      console.error(`Error getting number of files: ${err}`);
+      return 0;
+    }
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
