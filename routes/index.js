fe4a32f2824cb1ffd08e81c24c2b29ccb453479d
kurves <kavssimply@gmail.com>
2024-09-29 10:51:49 -0700

    Authenticate a user

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController.js';
 import UsersController from '../controllers/UsersController.js';
+import AuthController from '../controllers/AuthController.js';
 
 const router = Router();
 
