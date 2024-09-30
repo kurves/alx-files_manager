9c6639ffa694294c444d7ba78ec07afdced17704
kurves <kavssimply@gmail.com>
2024-09-29 22:51:28 -0700

    File publish/unpublish

--- routes/index.js
+++ routes/index.js
@@ -17,5 +17,8 @@ router.post('/files', FilesController.postUpload);
 router.get('/files/:id', FilesController.getShow);
 router.get('/files', FilesController.getIndex);
 
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+
 export default router;
 
