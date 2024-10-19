269f52ad44cc1fa6a0e55c95521915f98224aaf3
kurves <kavssimply@gmail.com>
2024-10-19 02:28:54 -0700

    fix eslint error

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import routes from './routes/index.js';
+import routes from './routes/index';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -7,5 +7,5 @@ const PORT = process.env.PORT || 5000;
 app.use(routes);
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
